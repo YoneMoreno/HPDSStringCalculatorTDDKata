4f610dfe78628de11db664b87c54314586580da3
YonePC <YonePC@YONE>
2017-12-16 14:28:01 +0000

    refactor: rename method to getStringsDelimitedBy and eliminated duplicated code

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,12 +3,13 @@ import static java.lang.Integer.*;
 public class Calculator {
     public int calculate(String number) {
         if (number.isEmpty()) return 0;
+
         if (number.contains(",")) {
-            String[] split = getStringsDelimitedByComma(number);
+            String[] split = getStringsDelimitedBy(",", number);
             return getAnIntSummingTwoNumbers(split);
         }
-        if(number.contains("\n")){
-            String[] split = number.split("\n");
+        if (number.contains("\n")) {
+            String[] split = getStringsDelimitedBy("\n", number);
             return getAnIntSummingTwoNumbers(split);
         }
         return parseInt(number);
@@ -18,7 +19,7 @@ public class Calculator {
         return parseInt(split[0]) + parseInt(split[1]);
     }
 
-    private String[] getStringsDelimitedByComma(String number) {
-        return number.split(",");
+    private String[] getStringsDelimitedBy(String delimiter, String number) {
+        return number.split(delimiter);
     }
 }
