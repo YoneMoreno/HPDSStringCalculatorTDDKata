4a26e92e48b18bad6b2cc7787648c79b61500347
YonePC <YonePC@YONE>
2017-12-16 14:04:38 +0000

    refactor: extract field to setup

--- Test/TestCalculate.java
+++ Test/TestCalculate.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -6,16 +7,21 @@ import static org.junit.Assert.*;
 
 public class TestCalculate {
 
+    private Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
     @Test
     public void emptyStringReturnsZero() {
-        Calculator calculator = new Calculator();
 
         assertEquals(0, calculator.calculate(""));
     }
 
     @Test
     public void givenSingleNumberExpectedReturnsTheSameNumber() {
-        Calculator calculator = new Calculator();
 
         assertEquals(27, calculator.calculate("27"));
     }
