559cc7a5dc833e7d8755f1de647fd93628a08e95
YonePC <YonePC@YONE>
2017-12-16 14:19:00 +0000

    refactor: extract method getStringsDelimitedByComma, extract method getAnIntSummingTwoNumber

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,10 +1,20 @@
+import static java.lang.Integer.*;
+
 public class Calculator {
     public int calculate(String number) {
         if (number.isEmpty()) return 0;
         if (number.contains(",")) {
-            String[] split = number.split(",");
-            return Integer.parseInt(split[0]) + Integer.parseInt(split[1]);
+            String[] split = getStringsDelimitedByComma(number);
+            return getAnIntSummingTwoNumbers(split);
         }
-        return Integer.parseInt(number);
+        return parseInt(number);
+    }
+
+    private int getAnIntSummingTwoNumbers(String[] split) {
+        return parseInt(split[0]) + parseInt(split[1]);
+    }
+
+    private String[] getStringsDelimitedByComma(String number) {
+        return number.split(",");
     }
 }
