05f7c8556181d8ae1e78a0f70341b019ced0d22c
YonePC <YonePC@YONE>
2017-12-16 14:16:02 +0000

    green: givenTwoNumberDelimitedByCommaExpectedTheSum

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,10 @@
 public class Calculator {
-    public int calculate(String s) {
-        if (s.isEmpty()) return 0;
-        return Integer.parseInt(s);
+    public int calculate(String number) {
+        if (number.isEmpty()) return 0;
+        if (number.contains(",")) {
+            String[] split = number.split(",");
+            return Integer.parseInt(split[0]) + Integer.parseInt(split[1]);
+        }
+        return Integer.parseInt(number);
     }
 }
